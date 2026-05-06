96524d99773b53916a019cc5e64229f262d093a5
tbirahim <gayethiernobirahim3@gmail.com>
2026-05-06 16:14:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ st.markdown("<div class='section-header'>Les Meilleurs</div>", unsafe_allow_html
 v_cols = st.columns(4)
 ventes = [
     {"n": "Voile Jersey", "p": "1.500", "i": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtowp8MI_nAZn_iJYDd3KFlcwXUuwvehdnAQ&s"},
-    {"n": "Parfum et Huile", "p": "3.500", "i": "https://fr.pngtree.com/freebackground/luxury-fragrance-and-perfume-bottles-at-night-parfum-oils-candle-light-photo_14534848.html"},
+    {"n": "Parfum et Huile", "p": "3.500", "i": "https://png.pngtree.com/thumb_back/fw800/background/20220930/pngtree-luxury-fragrance-and-perfume-bottles-at-night-parfum-oils-candle-light-photo-image_48968006.jpg"},
     {"n": "Pashmina", "p": "2.500", "i": "https://images.unsplash.com/photo-1606293926075-69a00dbfde81?w=400"},
     {"n": "Pince Premium", "p": "500", "i": "https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=400"}
 ]
